a735f0d77c5237cf0b427981ea3292eb35467712
zhangshuo <443494891@qq.com>
2019-12-23 11:42:42 +0800

    modify project structure

---
+++ blog/src/App.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react'
+import { Layout } from 'antd'
+
+const { Header, Content, Sider, Footer} = Layout;
+
+export default class App extends Component {
+	render() {
+		return (
+			<Layout>
+				<Header>
+					
+				</Header>
+				{/*  */}
+				<Sider>
+
+				</Sider>
+				{/*  */}
+				<Content>
+
+				</Content>
+				{/*  */}
+				<Footer>
+
+				</Footer>
+			</Layout>
+		)
+	}
+}
+
