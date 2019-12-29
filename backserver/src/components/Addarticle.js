24b0cd749a048833325ec9fb19af3b2dd3ea94a6
zhangshuo <443494891@qq.com>
2019-12-29 17:49:51 +0800

    modify addArticle

--- backserver/src/components/Addarticle.js
+++ backserver/src/components/Addarticle.js
@@ -1,10 +1,32 @@
 import React, { Component } from 'react'
+import { List, Form, Input} from 'antd'
+import Editor from 'for-editor'
 
 export default class Addarticle extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            value:''
+        }
+    }
+    handleChange = (value)=>{
+        this.setState({
+          value:value
+        });
+        console.log(value)
+    }
     render() {
         return (
             <div>
-                Addarticle
+                <List>
+                    <List.Item>
+                        <Form>
+                            <Form.Item label="标题"><Input/></Form.Item>
+                        </Form>
+                    </List.Item>
+                    <List.Item></List.Item>
+                </List>
+                <Editor toolbar={{h1: true,h2: true,h3: true,h4: true,img: true,link: true,code: true,preview: true,expand: true,undo: true,redo: true,save: true,subfield: true}} value={this.state.value} onChange={this.handleChange} />
             </div>
         )
     }
